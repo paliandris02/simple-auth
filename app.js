cf50128e42fa02d198c54a33e7b4be52106a787e
Páli András <pali.andris@gmail.com>
2024-03-19 16:54:29 +0100

    jwt

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const user = require("./routes/user");
+const cookieParser = require("cookie-parser");
 
 const app = express();
 
 const PORT = process.env.PORT || 4000;
 
 app.use(bodyParser.json());
-
+app.use(cookieParser());
 app.get("/", (req, res) => {
   res.json({ response: "API running" });
 });
--- routes/user.js
+++ routes/user.js
@@ -5,6 +5,10 @@ const Users = require("../mockDD");
 const User = require("../model/userModel");
 const UsersDB = new Users();
 
+router.get("/", async (req, res) => {
+  res.status(200).send("User endpoint");
+});
+
 router.get("/getAll", [], async (req, res) => {
   try {
     res.json({
@@ -34,6 +38,11 @@ router.post("/signup", [], async (req, res) => {
 
     jwt.sign(payload, "randomString", { expiresIn: 1000 }, (err, token) => {
       if (err) throw err;
+      res.cookie("jwt", token, {
+        httpOnly: true,
+        secure: true,
+        maxAge: 3600000,
+      });
       res.status(200).json({
         token,
       });
@@ -42,5 +51,11 @@ router.post("/signup", [], async (req, res) => {
     console.log(error.message);
     res.status(500).send("Error at signup");
   }
+});
+
+router.post("/login", async (req, res) => {
+  try {
+    res.send(req.cookies.jwt);
+  } catch (error) {}
 });
 module.exports = router;
